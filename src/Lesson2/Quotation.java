24635926d6d04c5ce390efc43dda00a22e6a4b2b
eogieglo <41413402+eogieglo@users.noreply.github.com>
2021-03-30 15:58:20 -0300

    Update Lesson2

---
+++ src/Lesson2/Quotation.java
@@ -0,0 +1,18 @@
+package Lesson2;
+
+
+
+public class Quotation {
+	
+		public static String Quote = "Welcome to Oracle, the new home of Java";
+		
+		public static void display() {
+			//Display the member variable here
+			System.out.println(Quote);
+		}
+		
+		public static void main(String[] args) {
+			Quotation.display();
+		}
+		
+}
